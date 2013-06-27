32fe5d28d570148406c6dd23b2a23b64e4bd2994
shroud <shroud@quakenet.org>
2013-06-27 06:47:12 +0200

    Remove TODO list from the script.

--- labspace.lua
+++ labspace.lua
@@ -15,14 +15,6 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
--- TODO
--- logging
--- vote info in !status
--- canonicalize channel names (or figure out if lua has case-insensitive dicts)
-
--- Ideas:
--- scientists vote on kills
-
 local BOTNICK = "labspace"
 local BOTACCOUNT = "labspace"
 local BOTACCOUNTID = 5022574
